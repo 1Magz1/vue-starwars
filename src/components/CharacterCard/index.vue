0ff4bfbc0415c776736f8f7f14b5f9edcaceb4cf
Maxim Chipurnov <maxim.chipurnoff@yandex.ru>
2021-07-13 12:56:03 +0700

    add character card

---
+++ src/components/CharacterCard/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="character-card">
+        <div class="character-card__img">
+            <img
+                :src="`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`"
+                alt="Character Image">
+        </div>
+        <h3 class="character-card__name">{{name}}</h3>
+        <span>{{id}}</span>
+        <button class="btn character-card__btn">Favorite</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        id: Number,
+    },
+};
+</script>
+
+<style></style>
