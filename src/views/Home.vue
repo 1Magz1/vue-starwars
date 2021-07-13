4077227fc6f3dd9eaa18ed03470457c90ee5b96f
Maxim Chipurnov <maxim.chipurnoff@yandex.ru>
2021-07-13 17:29:19 +0700

    add loader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,48 @@
 <template>
     <div class="home wrap">
-        <div class="home__content">
-            <CharacterCard
-                v-for="person in characters"
-                :key="person.name"
-                :name="person.name"
-                :id="person.url.split('/')[5]"
-            />
+        <div
+            v-if="characters.length"
+            class="home__wrap">
+            <nav class="home__nav" aria-label="page navigation">
+                <ul class="pagination home__pagination">
+                    <li
+                        :class="{'disabled': currentPage === 1}"
+                        class="page-item home__pagination-item">
+                        <a
+                            @click="currentPage--, getCharacters(currentPage)"
+                            class="page-link home__pagination-link" href="#">Previous</a>
+                    </li>
+                    <li
+                        v-for="page in MaxPage"
+                        :key="page"
+                        :class="{active: currentPage === page}"
+                        @click="currentPage = page, getCharacters(page)"
+                        class="page-item home__pagination-item">
+                        <a class="page-link home__pagination-link" href="#">{{page}}</a>
+                    </li>
+                    <li
+                        :class="{'disabled': currentPage === MaxPage}"
+                        class="page-item home__pagination-item">
+                        <a
+                            @click="currentPage++, getCharacters(currentPage)"
+                            class="page-link home__pagination-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="home__content">
+                <CharacterCard
+                    v-for="person in characters"
+                    :key="person.name"
+                    :name="person.name"
+                    :id="person.url.split('/')[5]"
+                />
+            </div>
         </div>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li
-                    :class="{'disabled': currentPage === 1}"
-                    class="page-item">
-                    <a
-                        @click="currentPage--, getCharacters(currentPage)"
-                        class="page-link" href="#">Previous</a>
-                </li>
-                <li
-                    v-for="page in MaxPage"
-                    :key="page"
-                    :class="{active: currentPage === page}"
-                    @click="currentPage = page, getCharacters(page)"
-                    class="page-item">
-                    <a class="page-link" href="#">{{page}}</a>
-                </li>
-                <li
-                    :class="{'disabled': currentPage === MaxPage}"
-                    class="page-item">
-                    <a
-                        @click="currentPage++, getCharacters(currentPage)"
-                        class="page-link" href="#">Next</a>
-                </li>
-            </ul>
-        </nav>
+        <div
+            v-else
+            class="loader">
+            <img src="@/assets/star-wars.jpg" alt="">
+        </div>
     </div>
 </template>
 
@@ -83,12 +92,40 @@ export default {
         display: flex;
         flex-wrap: wrap;
         justify-content: center;
-        margin-top: 25px;
         .character-card {
             flex-basis: 17.67%;
             margin-right: 20px;
             margin-bottom: 20px;
         }
+    }
+    &__nav {
+        display: flex;
+        justify-content: center;
+        margin-top: 25px;
+    }
+    &__pagination-link {
+        border-color: #000;
+        color: #000;
+        &:hover {
+            color: #FFEE58;
+            border-color: #FFEE58;
+            background-color: #000;
+        }
+        &:focus {
+            box-shadow: none;
+        }
+    }
+    &__pagination-item {
+        &.active {
+            background: #000;
+            a {
+                color: #FFEE58;
+                background-color: #000;
+                border-color: #000;
+
+            }
+        }
+
     }
 }
 </style>
